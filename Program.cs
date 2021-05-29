4420ac4db9ae8023a9f579183acaa712527a2673
Mike Witts <mike.witts@microsoft.com>
2021-05-29 21:38:32 +0100

    time span multiplications

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DatesAndTimesInDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var start =DateTimeOffset.UtcNow;
+            var end = start.AddSeconds(45);
+
+            TimeSpan difference = end - start;
+
+            System.Console.WriteLine(difference.TotalMilliseconds);
+        }
+    }
+}
