b05b8391c0c030be04797a11d807c61196cdefad
Mike Witts <mike.witts@microsoft.com>
2021-05-29 22:22:51 +0100

    add time expansion example

--- Program.cs
+++ Program.cs
@@ -16,22 +16,33 @@ namespace DatesAndTimesInDotNet
         // }
             
         static Calendar calendar = CultureInfo.InvariantCulture.Calendar;
+        // static void Main(string[] args)
+        // {
+        //     var start = new DateTimeOffset(2007, 12, 31, 0, 0, 0, TimeSpan.Zero);
+
+        //     var week = calendar.GetWeekOfYear(start.DateTime,
+        //     CalendarWeekRule.FirstFourDayWeek,
+        //     DayOfWeek.Monday
+        //     );
+
+        //     System.Console.WriteLine(week);
+
+        //     var isoWeek = ISOWeek.GetWeekOfYear(start.DateTime);
+        //     System.Console.WriteLine(isoWeek);
+
+        //     var isoWeekHack = GetIso8601WeekOfYear(start.DateTime);
+        //     System.Console.WriteLine(isoWeekHack);
+        // }
+
         static void Main(string[] args)
         {
-            var start = new DateTimeOffset(2007, 12, 31, 0, 0, 0, TimeSpan.Zero);
+            var contractDate = new DateTimeOffset(2020, 2, 29, 0, 0, 0, TimeSpan.Zero);
 
-            var week = calendar.GetWeekOfYear(start.DateTime,
-            CalendarWeekRule.FirstFourDayWeek,
-            DayOfWeek.Monday
-            );
+            System.Console.WriteLine(contractDate);
 
-            System.Console.WriteLine(week);
+            contractDate = ExtendContract(contractDate, 1);
 
-            var isoWeek = ISOWeek.GetWeekOfYear(start.DateTime);
-            System.Console.WriteLine(isoWeek);
-
-            var isoWeekHack = GetIso8601WeekOfYear(start.DateTime);
-            System.Console.WriteLine(isoWeekHack);
+            System.Console.WriteLine(contractDate);
         }
 
         public static int GetIso8601WeekOfYear(DateTime time)
@@ -43,6 +54,17 @@ namespace DatesAndTimesInDotNet
             }
 
             return calendar.GetWeekOfYear(time, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+        }
+
+        public static DateTimeOffset ExtendContract(DateTimeOffset current, int months)
+        {
+            var newContractDate = current.AddMonths(months).AddTicks(-1);
+            
+            
+            return new DateTimeOffset(newContractDate.Year,
+                                    newContractDate.Month,
+                                    DateTime.DaysInMonth(newContractDate.Year, newContractDate.Month),
+                                    23, 59, 59, current.Offset);
         }
     }
 }
